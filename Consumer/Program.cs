d188880b257db69e576be600b83e412f4c1530b1
FelipeCS <FelipeCS@tce.rj.gov.br>
2019-12-09 18:12:13 -0300

    .

---
+++ Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName = "teste", Password = "teste" };
+            using (var conexao = factory.CreateConnection())
+            {
+                using (var canal = conexao.CreateModel())
+                {
+                    canal.QueueDeclare(queue: "SIGFIS",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    var consumidor = new EventingBasicConsumer(canal);
+                    consumidor.Received += (modeal, ea) =>
+                    {
+                        try
+                        {
+                            var body = ea.Body;
+                            var mensagem = Encoding.UTF8.GetString(body);
+                            Console.WriteLine(mensagem);
+                            canal.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
+                       
+                    };
+
+                    canal.BasicConsume(queue: "SIGFIS",
+                        autoAck: false,
+                        consumer: consumidor);
+
+                    Console.WriteLine("Consumidor rodando...");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ Publisher/Program.cs
@@ -0,0 +1,37 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory() { HostName = "localhost", UserName= "teste", Password = "teste" };
+            using (var conexao = factory.CreateConnection())
+            {
+                using (var canal = conexao.CreateModel()) {
+                    canal.QueueDeclare(queue: "SIGFIS",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+
+                    Console.WriteLine("Escreva a mensagem:");
+                    string mensagem = Console.ReadLine();
+
+                    canal.BasicPublish(exchange: "",
+                        routingKey: "SIGFIS",
+                        basicProperties: null,
+                        body: Encoding.UTF8.GetBytes(mensagem));
+
+                   
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
